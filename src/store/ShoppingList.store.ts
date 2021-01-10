036187f53fea0f80151c249c096cae6d358e25ee
letmewin22 <46356822+letmewin22@users.noreply.github.com>
2021-01-10 16:39:22 +0200

    update store

--- src/store/ShoppingList.store.ts
+++ src/store/ShoppingList.store.ts
@@ -2,44 +2,62 @@ import {observable, action} from 'mobx'
 import axios from 'axios'
 import {IListItem} from '../interfaces/IListItem'
 
+const URL = 'https://mysterious-caverns-45772.herokuapp.com'
+
 class ShoppingList {
   @observable.shallow list: IListItem[] = []
 
   @action
   addItem = async (newItem: IListItem) => {
     if (newItem.value.trim().length > 0) {
-      const res = await axios.post('http://localhost:5000/api/list/', newItem)
+      try {
+        const res = await axios.post(`${URL}/api/list/`, newItem)
 
-      if (res.status === 201) {
-        this.list = [...this.list, res.data.body]
+        if (res.status === 201) {
+          this.list = [...this.list, res.data.body]
+        }
+      } catch (e) {
+        console.log(e.message)
       }
     }
   }
 
   @action
   removeItem = async (el: IListItem) => {
-    await axios.delete(`http://localhost:5000/api/list/${el._id}`)
-    this.list = this.list.filter(item => item !== el)
+    try {
+      await axios.delete(`${URL}/api/list/${el._id}`)
+      this.list = this.list.filter(item => item !== el)
+    } catch (e) {
+      console.log(e.message)
+    }
   }
 
   @action
   toggleCheckItem = async (el: IListItem) => {
-    const res = await axios.put(`http://localhost:5000/api/list/${el._id}`, {
-      checked: !el.checked ? (el.checked = true) : (el.checked = false),
-    })
+    try {
+      const res = await axios.put(`${URL}/api/list/${el._id}`, {
+        checked: !el.checked ? (el.checked = true) : (el.checked = false),
+      })
 
-    this.list = this.list.map(item => {
-      if (item._id === res.data._id) {
-        return (item = res.data)
-      }
-      return item
-    })
+      this.list = this.list.map(item => {
+        if (item._id === res.data._id) {
+          return (item = res.data)
+        }
+        return item
+      })
+    } catch (e) {
+      console.log(e.message)
+    }
   }
 
   @action
   getItems = async () => {
-    const {data} = await axios.get('http://localhost:5000/api/list/')
-    this.list = data
+    try {
+      const {data} = await axios.get(`${URL}/api/list/`)
+      this.list = data
+    } catch (e) {
+      console.log(e.message)
+    }
   }
 }
 
